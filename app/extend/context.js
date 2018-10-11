ff254bbe10c8541ffe2463cca82398e18ed0dbd7
William <williamzjb@163.com>
2018-10-11 15:22:09 +0800

    Update error status code

--- app/extend/context.js
+++ app/extend/context.js
@@ -10,7 +10,7 @@ module.exports = {
     this.state = this.state || {};
     this.state.user = this.state.user || {};
     const not_permitted = !this.state.user.roleId || !isAdmin(this.state.user);
-    if (not_permitted) { this.throw(404, errMsg); }
+    if (not_permitted) { this.throw(401, errMsg); }
     this.user = this.state.user;
   },
 };
