600bd791a26d1427cea297216882e1c5178444e3
William <williamzjb@163.com>
2019-01-18 11:35:52 +0800

    Update code style of es service

--- app/service/es.js
+++ app/service/es.js
@@ -3,20 +3,19 @@
 const Service = require('egg').Service;
 const elasticsearch = require('elasticsearch');
 
-let Client;
-
-class EsService extends Service {
-  get client() {
-    return Client;
-  }
-}
-
 module.exports = app => {
   const config = app.config.elasticsearch;
-  Client = new elasticsearch.Client({
+  const Client = new elasticsearch.Client({
     host: config.url,
     apiVersion: config.version,
     log: config.log || 'info',
   });
+
+  class EsService extends Service {
+    get client() {
+      return Client;
+    }
+  }
+
   return EsService;
 };
