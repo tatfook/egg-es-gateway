150d081c91842a03524503c0801d1117643e6d05
leopoldchen <placeintime@live.cn>
2019-12-04 15:21:02 +0800

    feat rename_folder

--- app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-require('newrelic');
-
-module.exports = app => {
-    if (app.config.env !== 'prod') {
-        console.log('Newrelic is disabled');
-        process.env.NEW_RELIC_ENABLED = false;
-    }
-};
