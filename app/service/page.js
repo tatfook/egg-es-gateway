d8d7e9bda4239b055f11081d82407226f1a389f3
Owen <253065903@qq.com>
2019-12-25 11:26:45 +0800

    fix：page cannot update bug

--- app/service/page.js
+++ app/service/page.js
@@ -40,9 +40,9 @@ class PageService extends Service {
     }
 
     get_page_DSL(DSL = {}) {
-        const url = this.ctx.getParams().id; // use url as id, not the document id
+        const id = this.ctx.getParams().id; // use url as id, not the document id
         DSL.query = {
-            match_phrase: { url },
+            term: { id },
         };
         return DSL;
     }
